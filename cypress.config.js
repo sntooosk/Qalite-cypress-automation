040da218fb2c67ad823ea8b6dec026d79d54f212
Juliano Cassimiro Dos Santos <Juliano.cassimiro@qualitydigital.global>
2025-12-09 15:43:01 -0300

    chore: clear credentials from env example

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,17 @@ require('dotenv').config()
 const { defineConfig } = require('cypress')
 const cucumber = require('cypress-cucumber-preprocessor').default
 
+const buildCredentials = () => ({
+  admin: {
+    email: process.env.ADMIN_EMAIL,
+    password: process.env.ADMIN_PASSWORD,
+  },
+  user: {
+    email: process.env.USER_EMAIL,
+    password: process.env.USER_PASSWORD,
+  },
+})
+
 module.exports = defineConfig({
   video: true,
   retries: {
@@ -10,6 +21,7 @@ module.exports = defineConfig({
   },
   defaultCommandTimeout: 120000,
   pageLoadTimeout: 180000,
+  env: buildCredentials(),
   e2e: {
     baseUrl: 'https://qa-lite.vercel.app',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx,feature}',
